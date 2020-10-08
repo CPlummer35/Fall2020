fbe8d5871125b56374c5d7c07a6ebb05e28bade5
Caiden Plummer <caidenplummer@caidens-mbp.home>
2020-10-08 11:56:02 -0600

    commit

--- AlgTheory1/Assignment2/mst.cpp
+++ AlgTheory1/Assignment2/mst.cpp
@@ -1,205 +1,112 @@
 // C++ program for Kruskal's algorithm to find Minimum Spanning Tree 
-// of a given connected, undirected and weighted graph 
 #include <iostream> 
 #include <fstream>
+#include "node.h"
+#include "LinkedList.h"
 using namespace std; 
 
-// a structure to represent a weighted edge in graph 
-class Edge 
-{ 
-	public: 
-	int src, dest, weight; 
-}; 
-
-// a structure to represent a connected, undirected 
-// and weighted graph 
-class Graph 
-{ 
-	public: 
-	// V-> Number of vertices, E-> Number of edges 
-	int V, E; 
-
-	// graph is represented as an array of edges. 
-	// Since the graph is undirected, the edge 
-	// from src to dest is also edge from dest 
-	// to src. Both are counted as 1 edge here. 
-	Edge* edge; 
-}; 
-
-// Creates a graph with V vertices and E edges 
-Graph* createGraph(int V, int E) 
-{ 
-	Graph* graph = new Graph; 
-	graph->V = V; 
-	graph->E = E; 
-
-	graph->edge = new Edge[E]; 
-
-	return graph; 
-} 
-
-// A structure to represent a subset for union-find 
-class subset 
-{ 
-	public: 
-	int parent; 
-	int rank; 
-}; 
-
-// A utility function to find set of an element i 
-// (uses path compression technique) 
-int find(subset subsets[], int i) 
-{ 
-	// find root and make root as parent of i 
-	// (path compression) 
-	if (subsets[i].parent != i) 
-		subsets[i].parent = find(subsets, subsets[i].parent); 
-
-	return subsets[i].parent; 
-} 
-
-// A function that does union of two sets of x and y 
-// (uses union by rank) 
-void Union(subset subsets[], int x, int y) 
-{ 
-	int xroot = find(subsets, x); 
-	int yroot = find(subsets, y); 
-
-	// Attach smaller rank tree under root of high 
-	// rank tree (Union by Rank) 
-	if (subsets[xroot].rank < subsets[yroot].rank) 
-		subsets[xroot].parent = yroot; 
-	else if (subsets[xroot].rank > subsets[yroot].rank) 
-		subsets[yroot].parent = xroot; 
-
-	// If ranks are same, then make one as root and 
-	// increment its rank by one 
-	else
-	{ 
-		subsets[yroot].parent = xroot; 
-		subsets[xroot].rank++; 
-	} 
-} 
-
-// Compare two edges according to their weights. 
-// Used in qsort() for sorting an array of edges 
-int myComp(const void* a, const void* b) 
-{ 
-	Edge* a1 = (Edge*)a; 
-	Edge* b1 = (Edge*)b; 
-	return a1->weight > b1->weight; 
-} 
-
-// The main function to construct MST using Kruskal's algorithm 
-void KruskalMST(Graph* graph) 
-{ 
-	int V = graph->V; 
-	Edge result[V]; // Tnis will store the resultant MST 
-	int e = 0; // An index variable, used for result[] 
-	int i = 0; // An index variable, used for sorted edges 
-
-	// Step 1: Sort all the edges in non-decreasing 
-	// order of their weight. If we are not allowed to 
-	// change the given graph, we can create a copy of 
-	// array of edges 
-	qsort(graph->edge, graph->E, sizeof(graph->edge[0]), myComp); 
-
-	// Allocate memory for creating V ssubsets 
-	subset *subsets = new subset[( V * sizeof(subset) )]; 
-
-	// Create V subsets with single elements 
-	for (int v = 0; v < V; ++v) 
-	{ 
-		subsets[v].parent = v; 
-		subsets[v].rank = 0; 
-	} 
-
-	// Number of edges to be taken is equal to V-1 
-	while (e < V - 1 && i < graph->E) 
-	{ 
-		// Step 2: Pick the smallest edge. And increment 
-		// the index for next iteration 
-		Edge next_edge = graph->edge[i++]; 
-
-		int x = find(subsets, next_edge.src); 
-		int y = find(subsets, next_edge.dest); 
-
-		// If including this edge does't cause cycle, 
-		// include it in result and increment the index 
-		// of result for next edge 
-		if (x != y) 
-		{ 
-			result[e++] = next_edge; 
-			Union(subsets, x, y); 
-		} 
-		// Else discard the next_edge 
-	} 
-
-	// print the contents of result[] to display the 
-	// built MST 
-	cout<<"Following are the edges in the constructed MST\n"; 
-	for (i = 0; i < e; ++i) 
-		cout<<result[i].src<<" -- "<<result[i].dest<<" == "<<result[i].weight<<endl; 
-	return; 
-} 
-
-// Driver code 
 int main() 
-{ 
-	/* Let us create following weighted graph 
-			10 
-		0--------1 
-		| \ | 
-	6| 5\ |15 
-		| \ | 
-		2--------3 
-			4 
-	int V = 4; // Number of vertices in graph 
-	int E = 5; // Number of edges in graph 
-	Graph* graph = createGraph(V, E); 
-
-
-	// add edge 0-1 
-	graph->edge[0].src = 0; 
-	graph->edge[0].dest = 1; 
-	graph->edge[0].weight = 10; 
-
-	// add edge 0-2 
-	graph->edge[1].src = 0; 
-	graph->edge[1].dest = 2; 
-	graph->edge[1].weight = 6; 
-
-	// add edge 0-3 
-	graph->edge[2].src = 0; 
-	graph->edge[2].dest = 3; 
-	graph->edge[2].weight = 5; 
-
-	// add edge 1-3 
-	graph->edge[3].src = 1; 
-	graph->edge[3].dest = 3; 
-	graph->edge[3].weight = 15; 
-
-	// add edge 2-3 
-	graph->edge[4].src = 2; 
-	graph->edge[4].dest = 3; 
-	graph->edge[4].weight = 4; 
-
-	KruskalMST(graph); 
-
-	*/
+{
+	LinkedList<string> linkedList = LinkedList<string>();
 	ifstream inf;
 	inf.open("distances.txt",ios::in); //open a file to perform read operation using file object
    	if (inf.is_open()){   //checking whether the file is open
     	string line;
+		/*
     	while(getline(inf, line))
 		{ //read data from file object and put it into string.
-        	if(line.)
+        	if(line.find("*") == std::string::npos)
+			
+			
+
       	}
+		*/
+		
+		getline(inf, line);
+		linkedList.InsertHead(line);
 
 
     	inf.close(); //close the file object.
-   }
 
+		linkedList.PrintList();
+		cout << endl;
+   }
+   
+
+  	/*linked list testing
+    // NULL
+    
+
+    // 43->NULL
+    linkedList.InsertHead(43);
+
+    // 76->43->NULL
+    linkedList.InsertHead(76);
+
+    // 76->43->15->NULL
+    linkedList.InsertTail(15);
+
+    // 76->43->15->44->NULL
+    linkedList.InsertTail(44);
+
+    // Print the list element
+    cout << "First Printed:" << endl;
+    linkedList.PrintList();
+    cout << endl;
+
+    // 76->43->15->44->100->NULL
+    linkedList.Insert(4, 100);
+
+    // 76->43->15->48->44->100->NULL
+    linkedList.Insert(3, 48);
+
+    // 22->76->43->15->48->44->100->NULL
+    linkedList.Insert(0, 22);
+
+    // Print the list element
+    cout << "Second Printed:" << endl;
+    linkedList.PrintList();
+    cout << endl;
+
+    // Get value of the second index
+    // It should be 43
+    cout << "Get value of the second index:" << endl;
+    Node<int> * get = linkedList.Get(2);
+    if(get != NULL)
+        cout << get->Value;
+    else
+        cout << "not found";
+    cout << endl << endl;
+
+    // Find the position of value 15
+    // It must be 3
+    cout << "The position of value 15:" << endl;
+    int srch = linkedList.Search(15);
+    cout << srch << endl << endl;
+
+    // Remove first element
+    cout << "Remove the first element:" << endl;
+    linkedList.Remove(0);
+    // 76->43->15->48->44->100->NULL
+    linkedList.PrintList();
+    cout << endl;
+
+    // Remove fifth element
+    cout << "Remove the fifth element:" << endl;
+    linkedList.Remove(4);
+    // 76->43->15->48->100->NULL
+    linkedList.PrintList();
+    cout << endl;
+
+    // Remove tenth element
+    cout << "Remove the tenth element:" << endl;
+    linkedList.Remove(9);
+    // Nothing happen
+    // 76->43->15->48->100->NULL
+    linkedList.PrintList();
+    cout << endl;
+
+	*/
 
 
 
